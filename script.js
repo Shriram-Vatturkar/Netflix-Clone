0041d80e7ef80affc770be5b3cc82c2cc34aa359
Shriram-Vatturkar <shriram.vatturkar@gmail.com>
2025-03-16 02:27:31 +0530

    Changes to be committed: 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var swiper = new Swiper('.movie-slider', {
     spaceBetween: 10,
     loop: false,
     centeredSlides: false,
+    watchOverflow: true, // Add this parameter to stop scrolling when the last slide is reached
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -23,10 +24,15 @@ var swiper = new Swiper('.movie-slider', {
             this.navigation.nextEl.classList.remove('swiper-button-disabled');
         }
 
-        if (this.isBeginning){
-             this.navigation.prevEl.classList.add('swiper-button-disabled');
+        if (this.isBeginning) {
+            this.navigation.prevEl.classList.add('swiper-button-disabled');
         } else {
             this.navigation.prevEl.classList.remove('swiper-button-disabled');
         }
     }
 });
+
+// Ensure the checkEnd function is bound to the swiper instance
+swiper.checkEnd = swiper.checkEnd.bind(swiper);
+swiper.on('init', swiper.checkEnd);
+swiper.on('slideChange', swiper.checkEnd);
